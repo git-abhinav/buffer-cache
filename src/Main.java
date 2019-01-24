154509195f7f17199f7e18f5440035f971d42e3b
Abhinav Kumar <abhinavk.git@gmail.com>
2019-01-24 23:40:34 +0530

    working queue

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,26 @@
 public class Main extends queue{
     public static void main(String args[]){
         queue q = new queue();
-//        q.enQueue(1);
-        q.enQueue(2);
-        q.enQueue(3);
+        q.enQueue(1);
+        q.showQueue();
+
+//        q.enQueue(2);
+//        q.showQueue();
+
+
+//        q.enQueue(3);
+//        q.showQueue();
+
+
+
+//        q.enQueue(4);
+//        System.out.println("----------");
         System.out.println(q.deQueue());
+//        q.showQueue();
+        System.out.println(q.deQueue());
+//        q.showQueue();
+        System.out.println(q.deQueue());
+
         System.out.println(q.deQueue());
 
     }
--- src/queue.java
+++ src/queue.java
@@ -2,54 +2,154 @@ public class queue extends node{
     node head;
     node tail;
     public queue(){
-        head = null;
-        tail = null;
+        head = tail = null;
     }
+
     public boolean isQueueEmpty(){
-        if(head == null && tail == null){
+        if(head == null && tail == null)
             return true;
-        }
-        else{
+        else
             return false;
-        }
     }
+
+
     public void enQueue(int blockNumber){
         node newNode = new node(blockNumber);
         if(isQueueEmpty() == true){
-            newNode.storeNext(newNode);
-            newNode.storePrevious(newNode);
-            head = newNode;
-            tail = newNode;
+            head = tail = newNode;
+            head.storeNext(head);
+            head.storePrevious(head);
         }
-        else{
+        else
+        {
             tail.storeNext(newNode);
             newNode.storePrevious(tail);
+            newNode.storeNext(head);
+            head.storePrevious(newNode);
             tail = newNode;
-            tail.storeNext(head);
-            head.storePrevious(tail);
         }
+    }
+    public void showQueue(){
+        if(isQueueEmpty() == true) {
+            System.out.println("Queue Empty");
+        }
+        else{
+            node tempNode = head;
+            do{
+                System.out.print(tempNode.getNodeValue());
+                tempNode = tempNode.getNext();
+            }while(tempNode!=head);
+        }
+        System.out.println("");
+    }
+    private int countElementInQueue(){
+        int count = 0;
+        if(isQueueEmpty()==false){
+            node tempNode = head;
+            do{
+                tempNode = tempNode.getNext();
+                count+=1;
+            }while(tempNode!=head);
+        }
+        return count;
     }
     public int deQueue(){
-        try{
-            if(head == null && tail ==null)
-                throw new Exception();
+        int value=-1;
+        if(isQueueEmpty()==true){
+            try{
+                throw new Exception("");
+            }
+            catch (Exception e){
+                System.out.println("Under-flow");
+                System.exit(1);
+            }
         }
-        catch(Exception e)
-        {
-            System.out.println("Exception occurred, Under-flow");
-            System.exit(1);
-        }
-        int blockNumber = head.getNodeValue();
-        head.getPrevious().storeNext(head.getNext().getPrevious());
-        head.getNext().storePrevious(head.getPrevious().getNext());
-        head = head.getNext();
-        if(head.getNext() == head) {
-            head.storeNext(null);
-            head.storePrevious(null);
-            tail.storeNext(null);
-            tail.storePrevious(null);
+        else if (countElementInQueue() == 1){
+            value = head.getNodeValue();
             head = tail = null;
         }
-        return blockNumber;
+        else{
+            value = head.getNodeValue();
+            tail.storeNext(head.getNext());
+            head.getNext().storePrevious(tail);
+            head = head.getNext();
+        }
+        return value;
     }
 }
+
+
+
+//public class queue extends node{
+//    node head;
+//    node tail;
+//    public queue(){
+//        head = null;
+//        tail = null;
+//    }
+//    public boolean isQueueEmpty(){
+//        if(head == null && tail == null){
+//            return true;
+//        }
+//        else{
+//            return false;
+//        }
+//    }
+//    public void enQueue(int blockNumber){
+//        node newNode = new node(blockNumber);
+//        if(isQueueEmpty() == true){
+//            newNode.storeNext(newNode);
+//            newNode.storePrevious(newNode);
+//            head = newNode;
+//            tail = newNode;
+//            head.storeNext(head);
+//            tail.storePrevious(head);
+//        }
+//        else{
+//            tail.storeNext(newNode);
+//            newNode.storePrevious(tail);
+//            newNode.storeNext(head);
+//            head.storePrevious(newNode);
+//            tail = newNode;
+//        }
+//    }
+//    public int deQueue(){
+//        try{
+//            if(head == null && tail ==null)
+//                throw new Exception();
+//        }
+//        catch(Exception e)
+//        {
+//            System.out.println("Exception occurred, Under-flow");
+//            System.exit(1);
+//        }
+//        int blockNumber = head.getNodeValue();
+//
+//        tail.storeNext(head.getNext());
+//        head.getNext().storePrevious(tail);
+//        head = head.getNext();
+//
+//
+//        if(head == tail) {
+//            head.storeNext(null);
+//            head.storePrevious(null);
+//            tail.storeNext(null);
+//            tail.storePrevious(null);
+//            head = tail = null;
+//        }
+//
+//        return blockNumber;
+//    }
+//    public void showQueue(){
+//        if(isQueueEmpty()==false)
+//        {
+//            node temp = head;
+//            do{
+//              System.out.print(temp.getNodeValue()+" - ");
+//              temp = temp.getNext();
+//            }while(temp != head);
+//            System.out.println("");
+//        }
+//    }
+//
+//}
