fe24621343692a2783b0e0e3bbf998434477f4c2
Abhinav Kumar <abhinavk.git@gmail.com>
2019-01-24 23:57:46 +0530

    working queue

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,9 @@ public class Main extends queue{
         queue q = new queue();
         q.enQueue(1);
         q.showQueue();
-
-//        q.enQueue(2);
-//        q.showQueue();
-
-
-//        q.enQueue(3);
-//        q.showQueue();
-
-
-
-//        q.enQueue(4);
-//        System.out.println("----------");
         System.out.println(q.deQueue());
-//        q.showQueue();
         System.out.println(q.deQueue());
-//        q.showQueue();
         System.out.println(q.deQueue());
-
         System.out.println(q.deQueue());
 
     }
