a45ddc3be2e8a8794f28f82425d424aa5fff5acb
Abhinav Kumar <abhinavk.git@gmail.com>
2019-01-26 23:04:14 +0530

    raw layout ready

--- src/bufferCache.java
+++ src/bufferCache.java
@@ -1,3 +1,15 @@
 public class bufferCache {
-    // some private data memebes
+    private queue lists[];
+    private int bufferCacheSize;
+    public bufferCache() {
+        lists = new queue[5];
+        bufferCacheSize = 4;
+    }
+    private int getHashValue(int blockNumber){
+        return (blockNumber % bufferCacheSize);
+    }
+    public void insertInCache(int blockNumber){
+        
+    }
+
 }
--- src/hashTable.java
+++ src/hashTable.java
@@ -1,3 +1,2 @@
 public class hashTable {
-    private
 }
